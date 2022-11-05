f63c6fbad654b02322d1fb0dd69fda502eb17a8a
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-11-05 07:58:23 -0700

    drafting array for updateEmpRole employee list choices

--- index.js
+++ index.js
@@ -7,37 +7,39 @@ let employees = []
 
 //create const objects to list dept names, role titles, employee everythings as choices in inquirer prompts
 db.promise().query('SELECT * FROM departments')
-.then(([rows]) => {
-    let deptNames = rows;
-    departments = deptNames.map(({ id, name }) => ({
-        name: name,
-        value: id
-    }))
-console.log("departments list = " + JSON.stringify(departments))})
-    
+    .then(([rows]) => {
+        let deptNames = rows;
+        departments = deptNames.map(({ id, name }) => ({
+            name: name,
+            value: id
+        }))
+        console.log("departments list = " + JSON.stringify(departments))
+    })
+
 db.promise().query('SELECT * FROM roles')
-.then(([rows]) => {
-    let roleTitles = rows;
-    roles = roleTitles.map(({ id, title }) => ({
-        name: title,
-        value: id
-    }))
-// console.log("roles list = " + JSON.stringify(roles))
-})
+    .then(([rows]) => {
+        let roleTitles = rows;
+        roles = roleTitles.map(({ id, title }) => ({
+            name: title,
+            value: id
+        }))
+        // console.log("roles list = " + JSON.stringify(roles))
+    })
 
 db.promise().query('SELECT * FROM employees')
-.then(([rows]) => {
-    let employeesArray = rows;
-    employees = employeesArray.map(({ id, first_name, last_name, email, department, role_id, manager_id}) => ({
-        id: id,
-        first_name: first_name,
-        last_name: last_name,
-        email: email,
-        department: department,
-        role_id: role_id,
-        manager_id: manager_id
-    }))
-console.log("employees list = " + JSON.stringify(employees))})
+    .then(([rows]) => {
+        let employeesArray = rows;
+        employees = employeesArray.map(({ id, first_name, last_name, email, department, role_id, manager_id }) => ({
+            id: id,
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            department: department,
+            role_id: role_id,
+            manager_id: manager_id
+        }))
+        console.log("employees list = " + JSON.stringify(employees))
+    })
 
 // main menu, ask user what they want to do in the app; if statements trigger individual functions
 const promptUser = () => {
@@ -60,47 +62,37 @@ const promptUser = () => {
             ]
         }
     ]).then((res) => {
-            if(res.start === 'View all departments')
-            {
-                viewAllDept()
-            }
-            else if (res.start === 'View all roles')
-            {
-                viewAllRoles()
-            }
-            else if (res.start === 'View all employees')
-            {
-                viewAllEmp()
-            }
-            else if (res.start === 'View all employees by department')
-            {
-                viewAllEmpDept()
-            }
-            else if (res.start === 'Add a department')
-            {
-                addDept()
-            }
-            else if (res.start === 'Add a role')
-            {
-                addRole()
-            }
-            else if (res.start === 'Add an employee')
-            {
-                addEmp()
-            }
-            else if (res.start === 'Update an employee role')
-            {
-                updateEmpRole()
-            }
-            else if (res.start === 'Remove a department')
-            {
-                removeDept()
-            }
-            else if (res.start === 'Quit')
-            {
-                quit()
-            }
-        })
+        if (res.start === 'View all departments') {
+            viewAllDept()
+        }
+        else if (res.start === 'View all roles') {
+            viewAllRoles()
+        }
+        else if (res.start === 'View all employees') {
+            viewAllEmp()
+        }
+        else if (res.start === 'View all employees by department') {
+            viewAllEmpDept()
+        }
+        else if (res.start === 'Add a department') {
+            addDept()
+        }
+        else if (res.start === 'Add a role') {
+            addRole()
+        }
+        else if (res.start === 'Add an employee') {
+            addEmp()
+        }
+        else if (res.start === 'Update an employee role') {
+            updateEmpRole()
+        }
+        else if (res.start === 'Remove a department') {
+            removeDept()
+        }
+        else if (res.start === 'Quit') {
+            quit()
+        }
+    })
 }
 
 //display full departments table: ids, names
@@ -127,7 +119,7 @@ const viewAllEmp = () => {
         console.table(res)
     })
     console.log('Use arrow keys to select a new prompt')
-    promptUser()
+    // promptUser()
 }
 
 //display employees in a selected department
@@ -140,28 +132,28 @@ function viewAllEmpDept() {
             message: 'Which department? (use arrow keys)',
             choices: departments
         }])
-    //display employees where deptartment = deptChoice
-    .then(res => {
-        // employees.filter(emp => emp.departments === res)
-        // console.log('emp = ' + emp)
+        //display employees where deptartment = deptChoice
+        .then(res => {
+            // employees.filter(emp => emp.departments === res)
+            // console.log('emp = ' + emp)
 
-        const sql = 'SELECT * FROM employees WHERE employees.department = (?)'
-        console.log(employees)
-        const params = [employees.filter(emp => emp.department === res.deptChoice)]
-        console.log(employees[0].department)
-        console.log("string-id " + res.deptChoice)
-        console.log("res = " + JSON.stringify(res))
-        console.log("params = " + params)
-        //remove the .then that this promise is inside of?
-        db.promise().query(sql, params, (rows) => {
-            if (err) {
-                console.log(err)
-            }
-            console.log("testing")
-            console.log(rows)
-            console.table(rows)
+            const sql = 'SELECT * FROM employees WHERE employees.department = (?)'
+            console.log(employees)
+            const params = [employees.filter(emp => emp.department === res.deptChoice)]
+            console.log(employees[0].department)
+            console.log("string-id " + res.deptChoice)
+            console.log("res = " + JSON.stringify(res))
+            console.log("params = " + params)
+            //remove the .then that this promise is inside of?
+            db.promise().query(sql, params, (rows) => {
+                if (err) {
+                    console.log(err)
+                }
+                console.log("testing")
+                console.log(rows)
+                console.table(rows)
+            })
         })
-    })
     // promptUser()
 }
 
@@ -192,23 +184,23 @@ const addDept = () => {
                     console.log('A name is needed.');
                     return false;
                 }
-            } 
+            }
         }
     ])
-    //insert into departments table
-    .then((res) => {
-        const sql = 'INSERT INTO departments (name) VALUES (?)'
-        const params = [res.newDeptName]
-        console.log('params = ' + JSON.stringify(params))
+        //insert into departments table
+        .then((res) => {
+            const sql = 'INSERT INTO departments (name) VALUES (?)'
+            const params = [res.newDeptName]
+            console.log('params = ' + JSON.stringify(params))
 
-        db.query(sql, params, (err, res) => {
-            if(err) {
-                console.log(err)
-            }
-            console.table(res)
+            db.query(sql, params, (err, res) => {
+                if (err) {
+                    console.log(err)
+                }
+                console.table(res)
+            })
+            console.log('New department added.')
         })
-        console.log('New department added.')
-    })
     // promptUser()
     viewAllDept()
 }
@@ -227,7 +219,7 @@ const addRole = () => {
                     console.log('A title is needed.');
                     return false;
                 }
-            } 
+            }
         },
         {
             type: 'input',
@@ -249,20 +241,20 @@ const addRole = () => {
             choices: departments
         }
     ])
-    //insert into roles table
-    //how to get department_id INT instead of department name?
-    .then((res) => {
-        const sql = 'INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)'
-        const params = [res.newRoleTitle, res.newRoleSalary, res.newRoleDept]
-        //with or without .id?
-        db.query(sql, params, (err, res) => {
-            if(err) {
-                console.log(err)
-            }
-            console.table(res)
+        //insert into roles table
+        //how to get department_id INT instead of department name?
+        .then((res) => {
+            const sql = 'INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)'
+            const params = [res.newRoleTitle, res.newRoleSalary, res.newRoleDept]
+            //with or without .id?
+            db.query(sql, params, (err, res) => {
+                if (err) {
+                    console.log(err)
+                }
+                console.table(res)
+            })
+            console.log('Role added.')
         })
-        console.log('Role added.')
-    })
     // promptUser()
 }
 
@@ -280,7 +272,7 @@ const addEmp = () => {
                     console.log('A name is needed.');
                     return false;
                 }
-            } 
+            }
         },
         {
             type: 'input',
@@ -315,45 +307,48 @@ const addEmp = () => {
             choices: roleChoices
         }
     ])
-    //insert into emmployees table
-    //HELP how to get role_id rather than title???
-    .then((res) => {
-        const sql = 'INSERT INTO employees (first_name, last_name, email, role_id) VALUES (?,?,?,?)'
-        const params = [res.newEmpFN.id, res.newEmpLN.id, res.newEmpEmail.id, res.newEmpRole.id]
-        db.query(sql, params, (err, res) => {
-            if(err) {
-                console.log(err)
-            }
-            console.table(res)
+        //insert into emmployees table
+        //HELP how to get role_id rather than title???
+        .then((res) => {
+            const sql = 'INSERT INTO employees (first_name, last_name, email, role_id) VALUES (?,?,?,?)'
+            const params = [res.newEmpFN.id, res.newEmpLN.id, res.newEmpEmail.id, res.newEmpRole.id]
+            db.query(sql, params, (err, res) => {
+                if (err) {
+                    console.log(err)
+                }
+                console.table(res)
+            })
+            console.log('Employee added.')
         })
-        console.log('Employee added.')
-    })
     promptUser()
 }
 
 const updateEmpRole = () => {
+    
     //ask user to select an employee
     //generate list of employees for inquirer prompt from employee table, save as employees
     //HELP is this SELECT query correct?
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'updateEmpChoice',
-            message: 'Which employee do you wish to update? (use arrow keys)',
-            choices: viewAllEmp()
-        },
-        {
-            type: 'list',
-            name: 'updateEmpRoleChoice',
-            message: 'What is the new role of the employee? (use arrow keys)',
-            choices: roles
-        }
-        ])
-        // .then((res) => {
-            //HELP how to get role_title vs. role_id, employee_id from fn/ln inquirer input?
-            // const sql = 'UPDATE employees SET role_title = ? WHERE employee_id = ?'
-            // const params = 
-}
+     viewAllEmp()
+        .then(([rows]) => {
+            let employeeRows = rows
+            employees = employeeRows.map(({ id, first_name, last_name }) => ({
+                name: `${first_name} ${last_name}`,
+                value: id
+            }))
+            console.log(JSON.stringify(employees))
+        })
+//run get all employees query, same thing; keep that as list so it's in the array; then update the mapped array and send it back (update db)
+    //         inquirer.prompt([
+    //             {
+    //                 type: 'list',
+    //                 name: 'employeeId',
+    //                 message: 'Which employee do you wish to update? (use arrow keys)',
+    //                 choices: employeeRows
+    //             }
+    //         ])
+            
+    //     })
+};
 
 const removeDept = () => {
     inquirer.prompt([
@@ -364,19 +359,19 @@ const removeDept = () => {
             choices: departments
         }
     ])
-    .then((res) => {
-        const sql = 'DELETE FROM departments WHERE department.id = (?)'
-        const params = [res.removeDept]
-        console.log('params = ' + JSON.stringify(params))
+        .then((res) => {
+            const sql = 'DELETE FROM departments WHERE department.id = (?)'
+            const params = [res.removeDept]
+            console.log('params = ' + JSON.stringify(params))
 
-        db.query(sql, params, (err, res) => {
-            if(err) {
-                console.log(err)
-            }
-            console.table(res)
+            db.query(sql, params, (err, res) => {
+                if (err) {
+                    console.log(err)
+                }
+                console.table(res)
+            })
+            console.log('The department has been removed.')
         })
-        console.log('The department has been removed.')
-    })
     promptUser()
 }
 
